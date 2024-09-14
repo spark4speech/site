ca2a210dd895ae51ef00f23ca94117b372548b3a
Shreyas Jain <shrys.jain@gmail.com>
2024-09-13 21:51:28 -0400

    update development banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,9 +1,9 @@
 export const Banner = () => {
   return (
-    <div className="py-3 text-center bg-[linear-gradient(to_right,rgba(252,214,255,0.7),rgba(41,216,255,0.7),rgba(255,253,128,0.7),rgba(248,154,191,0.7),rgba(252,214,255,0.7))]">
+    <div className="py-3 text-center bg-[linear-gradient(to_right,rgba(252,214,255,1),rgba(41,216,255,1),rgba(255,253,128,1),rgba(248,154,191,1),rgba(252,214,255,1))]">
       <div className="container">
         <p className="font-medium">
-          <span className="hidden sm:inline">
+          <span className="sm:inline">
             🚧 This website is currently under development. Some information may
             be inaccurate or incomplete. 🚧
           </span>
